390f8034f50b888f5c248941efd65e4a4652b4a6
Unknown <Kim.swope@yahoo.com>
2018-06-23 21:50:25 -0400

    made adjustments to API index.js file from articles to article

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,9 @@
-const router = require("express").Router();
-const articleRoutes = require("./articles");
+const router = require('express').Router();
+const articlesRoutes = require('./article');
+const savedRoutes = require('./saved');
 
-// Book routes
-router.use("/articles", articleRoutes);
+// articles routes
+router.use('/saved', savedRoutes);
+router.use('/article', articlesRoutes);
 
 module.exports = router;
